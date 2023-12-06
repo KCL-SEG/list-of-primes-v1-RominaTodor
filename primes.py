816cb9aa94be6b37e73b8f507d2cf85629b6b3b8
RominaTodor <146735764+RominaTodor@users.noreply.github.com>
2023-12-06 23:15:15 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+     list=[]
+     num=2
+
+     while len(list)<number_of_primes:
+          prime=True
+          for prime_num in range(2, int(num**0.5)+1):
+               if num%prime_num==0:
+                    prime=False
+                    break
+          if prime:
+               list.append(num)
+          num+=1
+     return list
